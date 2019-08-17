bcfb53970ff5d42ce132970ee30af4dc24d92e5b
Wattanit Hotrakool <wattanit.h@gmail.com>
2019-08-17 21:06:34 +0700

    Add demo auth

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Alert, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import { withCookies } from 'react-cookie';
 
 import NavigationBar from './Navbar';
@@ -7,11 +8,36 @@ import NavigationBar from './Navbar';
 class LoginPage extends Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            username: "",
+            password: "",
+            showAlert: false
+        }
     }
     
     login = ()=>{
-        this.props.cookies.set('auth_username', 'rorasa');
+        if(this.state.username==""){
+            this.setState({
+                showAlert: true
+            });
+            return;
+        }
+
+        this.props.cookies.set('auth_username', this.state.username);
         window.location="/dashboard/us"
+    }
+
+    onUsernameChange = (e)=>{
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onPasswordChange = (e)=>{
+        this.setState({
+            password: e.target.value
+        });
     }
 
     render(){
@@ -19,9 +45,25 @@ class LoginPage extends Component{
             <div>
                 <NavigationBar/>
 
-                <h1>Login</h1>
+                <Card className="LoginBox">
+                    <CardBody>
+                        <CardTitle><h2>Login</h2></CardTitle>
+                        <Form>
+                            {(this.state.showAlert)?<Alert color="danger">Wrong username or password</Alert>:null}
+                            <FormGroup>
+                                <Label>Username</Label>
+                                <Input type="text" value={this.state.username} name="username" id="username" onChange={this.onUsernameChange}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Password</Label>
+                                <Input type="password" value={this.state.password} name="password" id="password" onChange={this.onPasswordChange}/>
+                            </FormGroup>
+                        </Form>
+                        <Button color="primary" onClick={this.login}>Login</Button>
+                    </CardBody>
+                </Card>
 
-                <Button color="primary" onClick={this.login}>Login</Button>
+                
                 
             </div>
         )
