c5abbf715a38be7c5bc09971e5734d9bf644647d
Wattanit Hotrakool <wattanit.h@gmail.com>
2019-08-17 22:38:46 +0700

    Update landing page auth

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { withCookies } from 'react-cookie';
 
 import NavigationBar from './Navbar';
 
+
 class LandingPage extends Component{
     render(){
-        return(
-            <div>
-                <NavigationBar/>
-                <h2><strong>G</strong>lobally-indentified<br/>
-                 <strong>O</strong>ptimal <strong>P</strong>olicies for <strong>O</strong>peration of <strong>T</strong>rades</h2>
-                <a href="/login">Login</a><br/>
-            </div>
-        );
+        if(typeof(this.props.cookies.get('auth_username'))=="undefined"){
+            return(
+                <div>
+                    <NavigationBar/>
+                    <h2><strong>G</strong>lobally-indentified<br/>
+                     <strong>O</strong>ptimal <strong>P</strong>olicies for <strong>O</strong>peration of <strong>T</strong>rades</h2>
+                    <a href="/login">Login</a><br/>
+                </div>
+            );
+        }else{
+            window.location="/dashboard/us"
+        }
+        
     }
 }
 
-export default LandingPage;
+export default withCookies(LandingPage);
